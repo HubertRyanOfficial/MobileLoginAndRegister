ef115c9fa2d363eea0d3048c2fc278fe1e7b7537
Hubert Ryan <hubertryanofficial@gmail.com>
2020-08-17 19:56:40 -0300

    design screen login finished

--- src/routes/Login/index.js
+++ src/routes/Login/index.js
@@ -3,17 +3,67 @@ import React from 'react';
 // modules
 
 // components
-import { StatusBar } from 'react-native';
 
-import { Container } from './styles';
+import { StatusBar, Image } from 'react-native';
+
+import {
+  Container,
+  MiddleContainer,
+  MiddleTitle,
+  MiddleSubTitle,
+  InputsContainer,
+  EmailInput,
+  PasswordInput,
+  ForgotPasswordContainer,
+  ForgotPasswordText,
+  BottomContainer,
+  BottomButton,
+  Button,
+} from './styles';
 
 import Header from '../../components/Header';
+
+// images
+
+import NextIcon from '../../../assets/i_next.png';
 
 function Login() {
   return (
     <Container>
       <StatusBar backgroundColor="#fff" barStyle="dark-content" />
       <Header type={'signin'} />
+      <MiddleContainer>
+        <MiddleTitle>Welcome back,</MiddleTitle>
+        <MiddleSubTitle>Gave</MiddleSubTitle>
+
+        <InputsContainer>
+          <EmailInput
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Email address"
+            placeholderTextColor="#ddd"
+          />
+          <PasswordInput
+            autoCorrect={false}
+            autoCapitalize="none"
+            secureTextEntry
+            placeholder="Password"
+            placeholderTextColor="#ddd"
+          />
+        </InputsContainer>
+
+        <ForgotPasswordContainer onPress={() => {}}>
+          <ForgotPasswordText>Forgot password?</ForgotPasswordText>
+        </ForgotPasswordContainer>
+      </MiddleContainer>
+
+      <BottomContainer>
+        <Button onPress={() => {}}>
+          <BottomButton>
+            <Image source={NextIcon} />
+          </BottomButton>
+        </Button>
+      </BottomContainer>
     </Container>
   );
 }
--- src/routes/Login/styles.js
+++ src/routes/Login/styles.js
@@ -5,4 +5,67 @@ export const Container = styled.View`
   background-color: #fff;
 `;
 
+export const MiddleContainer = styled.View`
+  margin-top: 100px;
+  padding: 0px 20px;
+  flex: 1;
+`;
+
+export const MiddleTitle = styled.Text`
+  font-size: 25px;
+  letter-spacing: 2px;
+`;
+
+export const MiddleSubTitle = styled.Text`
+  font-size: 30px;
+  font-weight: bold;
+  letter-spacing: 2px;
+`;
+
+export const InputsContainer = styled.View`
+  margin-top: 30px;
+`;
+
+export const EmailInput = styled.TextInput`
+  border-bottom-width: 1px;
+  border-color: #ddd;
+  color: #000;
+  font-size: 16px;
+`;
+
+export const PasswordInput = styled.TextInput`
+  border-bottom-width: 1px;
+  border-color: #ddd;
+  color: #000;
+  font-size: 16px;
+  margin-top: 25px;
+`;
+
+export const ForgotPasswordContainer = styled.TouchableWithoutFeedback``;
+
+export const ForgotPasswordText = styled.Text`
+  margin-top: 300px;
+  font-size: 12px;
+  color: #000;
+`;
+
+export const BottomContainer = styled.View`
+  height: 100px;
+  background-color: #eee;
+`;
+
+export const BottomButton = styled.View`
+  background-color: #84bcda;
+  width: 60px;
+  height: 60px;
+  border-radius: 10px;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  right: 20px;
+  top: -30px;
+`;
+
+export const Button = styled.TouchableWithoutFeedback``;
+
 // Desenvolvido por Hubert Ryan
